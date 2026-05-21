7367020a4a1d78089e20e45c0e82d30b42f55bcb
santhi999 <nsanthipriya999@gmail.com>
2026-05-20 23:54:08 -0400

    commit

--- jscript.js
+++ jscript.js
@@ -142,7 +142,7 @@ deduct 10 percent of the total points possible from their score for that assignm
 //Function 3
 
 function submission_Datecheck() {
-    //retrieve the record with matching assignment id and do date check
+    //retrieve the record with matching assignment id and do  date check
     let submissions = LearnerSubmissions.map(sub => {
         const assignment = AssignmentGroup.assignments.find(a => a.id === sub.assignment_id);
         if (!assignment) {
